adbc02207a0d9239f34bc0962fdbde79b234078c
Garrett <garres4@vt.edu>
2022-01-11 11:14:38 -0700

    might break things by deleting master key

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/hello', to: 'application#hello_world'
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   get '*path',
       to: 'fallback#index',
