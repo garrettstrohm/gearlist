b2c8bbb92587be6c562ea657d8d54edc99bde0af
Garrett <garres4@vt.edu>
2022-01-13 16:45:20 -0700

    navbar and side drawer are formatted

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,7 @@
 class TripsController < ApplicationController
+
+    def index
+        render json: current_user.trips, status: :ok
+    end
+
 end
---
+++ client/src/trip/TripCardContainer.js
@@ -0,0 +1,15 @@
+import Container from '@mui/material/Container';
+import TripCard from './TripCard';
+
+function TripCardContainer() {
+    
+    return (
+        <>
+            <Container>
+                {tripCardList}
+            </Container>
+        </>
+    )
+}
+
+export default TripCardContainer
