9b9e04e0cf1d22ae04813a09551dfdfb1daaa912
Garrett <garres4@vt.edu>
2022-01-13 11:38:38 -0700

    removed password tokens from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_162252) do
+ActiveRecord::Schema.define(version: 2022_01_13_183720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,8 +84,6 @@ ActiveRecord::Schema.define(version: 2022_01_13_162252) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.string "password_reset_token"
-    t.datetime "password_reset_sent_at", precision: 6
     t.string "recovery_password_digest"
   end
 
