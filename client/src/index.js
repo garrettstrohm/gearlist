75aa5939be4f8a8cf5559708c771cb33962a2356
Garrett <garres4@vt.edu>
2022-01-24 12:44:05 -0700

    formatted for production...maybe

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,16 @@
-import React, {createContext} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import store from "./store";
 import { Provider } from "react-redux"
-import actionCable from 'actioncable'
 
-// const CableApp = {}
-// CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
-// export const ActionCableContext = createContext()
+
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-    {/* <ActionCableContext.Provider value={CableApp.cable}> */}
       <App />
-      {/* </ActionCableContext.Provider> */}
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
