f93c26434c1f3bdc114b2280fa24429abc042ae3
Garrett <garres4@vt.edu>
2022-01-24 17:12:17 -0700

    adventure item modals and functions hooked up

--- client/src/item/AdventureItemCard.js
+++ client/src/item/AdventureItemCard.js
@@ -9,11 +9,22 @@ import Checkbox from '@mui/material/Checkbox';
 import {useDispatch, useSelector} from 'react-redux'
 import { setAllAdventureItems } from './itemSlice';
 import {useState} from 'react'
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import ItemDetailModal from './ItemDetailModal';
 
 function AdventureItemCard({item, handleDelete}) {
     const [cardItem, setCardItem] = useState({
         acquired: item.acquired
     })
+    const [toggle, setToggle] = useState(true)
+    const [form, setForm] = useState({
+        quantity: item.quantity
+    })
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const dispatch = useDispatch()
     const adventureItems = useSelector(state => state.items.adventureItems)
 
@@ -42,21 +53,75 @@ function AdventureItemCard({item, handleDelete}) {
             setCardItem({acquired: data.acquired})
         })
     }
+
+    function handleInputSubmit(e){
+        e.preventDefault()
+
+        const configObj = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        }
+
+        fetch(`/adventure_items/${item.id}`, configObj)
+        .then(r => r.json())
+        .then(data => {
+            const updatedItems = adventureItems.map(item => {
+                if(item.id === data.id){
+                    return data
+                } else {
+                    return item
+                }
+            })
+            dispatch(setAllAdventureItems(updatedItems))
+        })
+    }
+
+    function handleChange(e){
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
     if(item === null){
         return null
     } else {
     return (
         <div>
+            <ItemDetailModal open={open} handleClose={handleClose} item={item.item} acquired={item.acquired} quantity={item.quantity}/>
             <Box sx={{ minWidth: 275, marginTop: '3px' }}>
                 <Card variant="outlined">
                     <React.Fragment>
                         <CardContent>
-                            <Typography variant="h5" component="div">
-                                {item.item.name}
+                            <Typography variant="h5" component="div" onClick={handleOpen} sx={{cursor: 'pointer'}}>
+                                {item.item ? item.item.name : null}
                             </Typography>
-                            <Typography variant="body2">
+                            {toggle ? <Typography variant="body2" onDoubleClick={() => setToggle(false)}>
                                 Quantity: {item.quantity}
-                            </Typography>
+                            </Typography> :
+                            <Stack direction='row' spacing={5}>
+                                Quantity:
+                            <TextField
+                                size='small'
+                                variant='standard'
+                                type='number'
+                                value={form.quantity}
+                                name='quantity'
+                                onChange={handleChange}
+                                onKeyDown={e => {
+                                    if(e.key === 'Enter'){
+                                        handleInputSubmit(e)
+                                        setToggle(true)
+                                    } else if (e.key === 'Escape'){
+                                        setToggle(true)
+                                    }
+                                }}
+                                id='quantity'
+                            />
+                            </Stack> }
                             Acquired: <Checkbox checked={cardItem.acquired} onChange={() => handleUpdate(item.id)} inputProps={{ 'aria-label': 'controlled' }}/>
                         </CardContent>
                         <CardActions>
