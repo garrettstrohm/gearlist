48bcd56ba1caecda1ced97340d9ba166cd461ec7
Garrett <garres4@vt.edu>
2022-01-26 16:53:03 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     def create
         user = User.create!(user_params)
         session[:user_id] = user.id
-        UserMailer.welcome_email(user).deliver_later
         render json: user, status: :created
         end
     end
