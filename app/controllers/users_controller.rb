b392c473f75960f75af152a211e64c0c7a317f59
Garrett <garres4@vt.edu>
2022-01-26 17:10:45 -0700

    please work :(

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
         user = User.create!(user_params)
         session[:user_id] = user.id
         render json: user, status: :created
-        end
     end
 
     def update
